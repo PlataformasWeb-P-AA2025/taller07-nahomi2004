59a7f75ea6583aa177d08932bff45d106fd2ebb9
nahomi2004 <nahomicabrera2004@gmail.com>
2025-05-20 09:58:40 -0500

    Ingreso de datos casi listo

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -18,9 +18,38 @@ engine = create_engine(cadena_base_datos)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-clubs_csv = pd.read_csv("data/datos_clubs.txt")
+# Accedo al txt usando pandas
+# Especifico que no tiene encabezados para que la primera linea
+# no sea tomada como encabezado
+clubs_csv = pd.read_csv("data/datos_clubs.txt", delimiter=';', header=None)
 
-print(clubs_csv)
+# Reviso si esta tomando bien los datos
+# print(clubs_csv)
+# Ahora los encabezados son 0, 1 y 2
+# 0 es el nombre, 1 el deporte, 2 el anio de fundacion
+# print(clubs_csv[0])
+# print(clubs_csv[0][0])
+
+# Guardo a los clubs
+# Recorrer cada fila del DataFrame y crear objetos Club
+for index, row in clubs_csv.iterrows():
+    club = Club(
+        nombre=row[0],
+        deporte=row[1],
+        fundacion=int(row[2])  # Aseguramos que sea entero
+    )
+    session.add(club)
+
+# Hago lo mismo para jugadores
+jugadores_csv = pd.read_csv("data/datos_jugadores.txt", delimiter=';', header=None)
+
+for index, row in jugadores_csv.iterrows():
+    jugador = Jugador(
+        nombre=row[0],
+        dorsal=row[1],
+        posicion=int(row[2])  # Aseguramos que sea entero
+    )
+    session.add(jugador)
 
 # se confirma las transacciones
 session.commit()
