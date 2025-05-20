a22c033d2ba4c102e425522258a82ab3444b7730
nahomi2004 <nahomicabrera2004@gmail.com>
2025-05-20 09:28:34 -0500

    Creacion de los codigos

---
+++ ejemplo02/ingreso_datos.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# se importa la clase(s) del 
+# archivo genera_tablas
+from genera_tablas import Club, Jugador
+
+# se importa información del archivo configuracion
+from configuracion import cadena_base_datos
+import pandas as pd
+
+# se genera enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+clubs_csv = pd.read_csv("data/datos_clubs.txt")
+
+print(clubs_csv)
+
+# se confirma las transacciones
+session.commit()
